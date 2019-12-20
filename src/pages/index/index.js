f4fc2237ba3292032d434ec5d4cc420252de4f7d
SadCat88 <SadCat88@yandex.ru>
2019-12-20 14:03:00 +0300

    Логика движения шарика с ракеткой. Конец игры.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -13,141 +13,211 @@ import { getAfterContent } from '../../assets/js/getAfterContent.js';
 // временные стили
 // import "../../assets/scss/temporality.scss";
 
-// ================================================================== index.js
+// ==================================================================== index.js
+// =============================================================================
 ('use strict');
 
-// variables color ==================================================
-let $pageBgColor = window.getComputedStyle(
-  document.querySelector('#pageBgColor'),
-  ':after'
-).content;
-let $canvasBgColor = window.getComputedStyle(
-  document.querySelector('#canvasBgColor'),
-  ':after'
-).content;
-let $panelBgColor = window.getComputedStyle(
-  document.querySelector('#panelBgColor'),
-  ':after'
-).content;
+// === variables color =========================================================
+let $pageBgColor = getAfterContent('#pageBgColor');
+let $canvasBgColor = getAfterContent('#canvasBgColor');
+let $panelBgColor = getAfterContent('#panelBgColor');
 let $canvasFigureBg = getAfterContent('#canvasFigureBg');
+let $canvasText = getAfterContent('#canvasText');
 
-console.log($pageBgColor);
-console.log($canvasBgColor);
-console.log($panelBgColor);
-console.log($canvasFigureBg);
-
-// canvas ============================================================
+// === canvas ==================================================================
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
-// === определение стартовой позиции мячика
-// относительно размеров игрового поля
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-
-// === шаг движения за кадр
-let dx = 1;
-let dy = -1;
-
-// === радиус шара
+// ====== мячик
+// === координаты мячика
+let ball_x = canvas.width / 2;
+let ball_y = canvas.height - 30;
+// === ускорение движения
+let ballSpeed = 1.025;
+// === направление движения по осям
+let ball_dx = 1;
+let ball_dy = -1;
+// === радиус мячика
 let ballRadius = 10;
 
 // === ракетка
 let padHeight = 10;
 let padWidth = 75;
-let padX = canvas.width / 2;
+let pad_x = canvas.width / 2 - padWidth / 2;
+let pad_y = canvas.height - padHeight;
+let padLeftSide = pad_x;
+let padRightSide = pad_x + padWidth;
+let padTopSide = pad_y;
 
-// отрисовка ракетки
+
+// === флаг события - конец игры
+let gameOver = false;
+
+// === размеры игрового экрана
+let screen_x = 0;
+let screen_y = 0;
+let screenWidth = canvas.width;
+let screenHeight = canvas.height;
+
+let screenLeftSide = 0;
+let screenRightSide = canvas.width;
+let screenTopSide = 0;
+let screenBottomSide = canvas.height;
+
+/**
+ * Позволяет узнать длину строки в пикселях
+ *
+ * @deprecated <pre>
+ * Данная функция работает только с конкретным моноширинным шрифтом
+ * "Pixel Emulator"
+ * </pre>
+ * @param {string} string - некая строка
+ * @param {number} fontSize - размер шрифта
+ * @returns {number}
+ */
+const howStringWidthPX = (string, fontSize) => {
+  let qtySymbols = string.length;
+  // Посчитать количество символов
+  let qtySpaces = qtySymbols - 1;
+  // Посчитать количество межсимвольных отступов
+
+  let widthSymbol = fontSize * 0.68;
+  // Посчитать размер символа в пикселях
+  let widthSpace = fontSize * 0.1;
+  // Посчитать размер отступа в пикселях
+
+  return qtySymbols * widthSymbol + qtySpaces * widthSpace;
+  // Узнать длину строки в пикселях
+};
+
+// === отрисовка ракетки
 const drawPad = () => {
   ctx.beginPath();
-  ctx.rect(padX - padWidth / 2, canvas.height - padHeight, padWidth, padHeight);
+  ctx.rect(pad_x, pad_y, padWidth, padHeight);
   ctx.fillStyle = $canvasFigureBg;
   ctx.fill();
   ctx.closePath();
 };
 
-// отрисовка шарика
+// === отрисовка шарика
 const drawBall = () => {
   ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI * 2, false);
+  ctx.arc(ball_x, ball_y, ballRadius, 0, Math.PI * 2, false);
   ctx.fillStyle = $canvasFigureBg;
   ctx.fill();
   ctx.closePath();
 };
 
 /**
- * Отрисовывает 1 кадр движения шарика за каждый свой вызов
+ * Рисует 1 кадр движения шарика за каждый свой вызов
  *
  * @deprecated
- * Учитывает столкновения и меняет траекторию
+ * Проверяет всю логику движения объектов на игровом поле
  */
 const draw = () => {
-  // очистка предыдущего кадра
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // === очистка предыдущего кадра
+  ctx.clearRect(screen_x, screen_y, screenWidth, screenHeight);
 
-  // смещение шарика за каждый кадр
-  x += dx;
-  y += dy;
+  // === смещение шарика за каждый кадр
+  ball_x += ball_dx;
+  ball_y += ball_dy;
 
-  // отскок от правой стенки
-  if (x + ballRadius > canvas.width) {
-    dx = -dx;
+  // === отскок от правой стенки
+  if (ball_x + ballRadius > screenRightSide) {
+    ball_dx = -ball_dx;
   }
 
-  // отскок от левой стенки
-  if (x - ballRadius < 0) {
-    dx = -dx;
+  // === отскок от левой стенки
+  if (ball_x - ballRadius < screenLeftSide) {
+    ball_dx = -ball_dx;
   }
 
-  // отскок от верхней стенки
-  if (y - ballRadius < 0) {
-    dy = -dy;
+  // === отскок от верхней стенки
+  if (ball_y - ballRadius < screenTopSide) {
+    ball_dy = -ball_dy;
   }
 
-  // отскок от нижней стенки
-  if (y + ballRadius > canvas.height) {
-    dy = -dy;
+  // === касание нижней стенки - Game over
+  if (ball_y + ballRadius > screenBottomSide) {
+    clearInterval(interval);
+    console.log(`позиция мячика y=${ball_y}`);
+    console.log(`позиция нижнего края экрана ${screenBottomSide}`);
+    console.log(`позиция верхнего края доски ${padTopSide}`);
+    console.log(`позиция мячика x=${ball_x}`);
+    console.log(`позиция доски left=${padLeftSide} right=${padRightSide}`);
+    gameOver = true;
   }
 
-  // перемещение ракетки влево
-  if (leftDown == true && padX >= 0 + padWidth / 2) {
-    padX -= 2;
+  // === отбив ракеткой
+  if (ball_y + ballRadius == padTopSide) {
+    if (ball_x >= padLeftSide && ball_x <= padRightSide) {
+      ball_dy = -ball_dy;
+    }
   }
 
-  // перемещение ракетки вправо
-  if (rightDown == true && padX <= canvas.width - padWidth / 2) {
-    padX += 2;
+  // === Game over
+  if (gameOver == true) {
+    let fontSize = 50;
+    let string = 'Game over';
+    let stringLength = howStringWidthPX(string, fontSize);
+    let string_x = canvas.width / 2 - stringLength / 2;
+    let string_y = 100;
+
+    ctx.fillStyle = $canvasText;
+    ctx.font = `${fontSize}px PixelEmulator`;
+    ctx.fillText(string, string_x, string_y);
   }
 
-  // отрисовка кадра
+  // === перемещение ракетки влево
+  if (leftArrowDown == true && padLeftSide >= screenLeftSide) {
+    pad_x -= 2;
+    padLeftSide -= 2;
+    padRightSide -= 2;
+  }
+
+  // === перемещение ракетки вправо
+  if (rightArrowDown == true && padRightSide <= screenRightSide) {
+    pad_x += 2;
+    padLeftSide += 2;
+    padRightSide += 2;
+  }
+
+  // === отрисовка кадра
   drawBall();
   drawPad();
 };
 
-// бинд клавиш ===================================
-let leftDown = false;
-let rightDown = false;
+// === бинд клавиш =============================================================
+// === флаг события - нажатие кнопок
+let leftArrowDown = false;
+let rightArrowDown = false;
 
+// === нажатие кнопки
 let keyDown = element => {
   if (element.key == 'ArrowLeft') {
-    leftDown = true;
+    leftArrowDown = true;
   }
   if (element.key == 'ArrowRight') {
-    rightDown = true;
+    rightArrowDown = true;
   }
 };
+
+// === отпускание кнопки
 let keyUp = element => {
   if (element.key == 'ArrowLeft') {
-    leftDown = false;
+    leftArrowDown = false;
   }
   if (element.key == 'ArrowRight') {
-    rightDown = false;
+    rightArrowDown = false;
   }
 };
 
+// === запуск нужных функций на нужные события
 document.onkeydown = keyDown;
+// кнопка нажата
 document.onkeyup = keyUp;
-// ================================================
+// кнопка отпущена
+// =============================================================================
 
-setInterval(draw, 10);
-// запуск функции отрисовки в заданный интервал - скорость
+// === запуск функции отрисовки в заданный интервал - скорость
+let interval = setInterval(draw, 10);
